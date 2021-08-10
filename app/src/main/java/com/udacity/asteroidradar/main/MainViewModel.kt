47e6abe273345394534d8b1e5b4f4b48c909f162
Josue Martinez <46319076+josue-martinez@users.noreply.github.com>
2021-08-10 12:00:12 -0400

    Add database Entities

--- app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
+++ app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
@@ -1,18 +1,47 @@
 package com.udacity.asteroidradar.main
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.udacity.asteroidradar.Constants.API_KEY
+import com.udacity.asteroidradar.PictureOfDay
+import com.udacity.asteroidradar.api.AsteroidApiService
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 
 class MainViewModel(repository: AsteroidRepository) : ViewModel() {
+
+    // The internal MutableLiveData String that stores the status of the most recent request
+    private val _pictureOfDay = MutableLiveData<PictureOfDay>()
+
+    // The external immutable LiveData for the request status String
+    val pictureOfDay: LiveData<PictureOfDay>
+        get() = _pictureOfDay
+
     init {
-        Log.i("MainViewModel", "MainViewModel Created $repository!!" )
+        viewModelScope.launch {
+            updatePictureOfDay()
+        }
     }
 
-    override fun onCleared() {
-        super.onCleared()
-        Log.i("MainViewModel", "MainViewModel Destroyed!!" )
+    private suspend fun updatePictureOfDay() {
+        withContext(Dispatchers.IO) {
+            try {
+                _pictureOfDay.postValue(
+                    AsteroidApiService.AsteroidApi.retrofitService.getPictureOfDay(API_KEY)
+                )
+            } catch (err: Exception) {
+                Log.e("refreshPictureOfDay", err.printStackTrace().toString())
+            }
+        }
+
     }
+
+
 }
 
 
