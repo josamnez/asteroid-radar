e1bc0c69e47443b616b5c7a34b9b5481be23ffdd
Josue Martinez <46319076+josue-martinez@users.noreply.github.com>
2021-08-23 10:35:43 -0400

    Update Room.kt and add Singleton

--- app/src/main/java/com/udacity/asteroidradar/database/Room.kt
+++ app/src/main/java/com/udacity/asteroidradar/database/Room.kt
@@ -1,3 +1,40 @@
 package com.udacity.asteroidradar.database
 
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.room.*
 
+
+@Dao
+interface AsteroidDao {
+
+    @Query("SELECT * FROM asteroids_table ORDER BY closeApproachDate DESC")
+    fun getAsteroids(): LiveData<List<DatabaseAsteroid>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAll(vararg asteroid: DatabaseAsteroid)
+}
+
+@Database(entities = [DatabaseAsteroid::class], version = 1)
+abstract class AsteroidsDatabase : RoomDatabase() {
+    abstract val asteroidDao: AsteroidDao
+}
+
+/**
+ *  Singleton Pattern that guarantees a class has one instance only and
+ *  a global point of access to it provided by that class.
+ */
+
+private lateinit var INSTANCE: AsteroidsDatabase
+
+fun getDatabase(context: Context): AsteroidsDatabase {
+    synchronized(AsteroidsDatabase::class.java){
+        if (!::INSTANCE.isInitialized) {
+            INSTANCE = Room.databaseBuilder(context.applicationContext,
+                AsteroidsDatabase::class.java,
+                "asteroids").build()
+        }
+    }
+
+    return INSTANCE
+}
