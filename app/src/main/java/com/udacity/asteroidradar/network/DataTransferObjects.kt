1ed950d8d87fe18eda09bc48c3f43dd3f439d9d6
Josue Martinez <46319076+josue-martinez@users.noreply.github.com>
2021-08-23 10:11:30 -0400

    Add Database

--- app/src/main/java/com/udacity/asteroidradar/network/DataTransferObjects.kt
+++ app/src/main/java/com/udacity/asteroidradar/network/DataTransferObjects.kt
@@ -1,45 +1,47 @@
 package com.udacity.asteroidradar.network
 
-//
-//
-///**
-// * DataTransferObjects go in this file. These are responsible for parsing responses from the server
-// * or formatting objects to send to the server. You should convert these to domain objects before
-// * using them.
-// */
-//
-//@JsonClass(generateAdapter = true)
-//data class NetworkAsteroidContainer(val asteroids: List<NetworkAsteroid>)
-//
-///**
-// * Videos represent a devbyte that can be played.
-// */
-//@JsonClass(generateAdapter = true)
-//data class NetworkAsteroid(
-//    val id: Long,
-//    val codename: String,
-//    val closeApproachDate: String,
-//    val absoluteMagnitude: Double,
-//    val estimatedDiameter: Double,
-//    val relativeVelocity: Double,
-//    val distanceFromEarth: Double,
-//    val isPotentiallyHazardous: Boolean
-//    )
-//
-///**
-// * Convert Network results to database objects
-// */
-//fun NetworkAsteroidContainer.asDomainModel(): List<Asteroid> {
-//    return asteroids.map {
-//        Asteroid(
-//            id = it.id,
-//            codename = it.codename,
-//            closeApproachDate = it.closeApproachDate,
-//            absoluteMagnitude = it.absoluteMagnitude,
-//            estimatedDiameter = it.estimatedDiameter,
-//            relativeVelocity = it.relativeVelocity,
-//            distanceFromEarth = it.distanceFromEarth,
-//            isPotentiallyHazardous = it.isPotentiallyHazardous
-//        )
-//    }
-//}
+import com.squareup.moshi.JsonClass
+import com.udacity.asteroidradar.Asteroid
+
+
+/**
+ * DataTransferObjects go in this file. These are responsible for parsing responses from the server
+ * or formatting objects to send to the server. You should convert these to domain objects before
+ * using them.
+ */
+
+@JsonClass(generateAdapter = true)
+data class NetworkAsteroidContainer(val asteroids: List<NetworkAsteroid>)
+
+/**
+ * Asteroids that can be showed
+ */
+@JsonClass(generateAdapter = true)
+data class NetworkAsteroid(
+    val id: Long,
+    val codename: String,
+    val closeApproachDate: String,
+    val absoluteMagnitude: Double,
+    val estimatedDiameter: Double,
+    val relativeVelocity: Double,
+    val distanceFromEarth: Double,
+    val isPotentiallyHazardous: Boolean
+    )
+
+/**
+ * Convert Network results to database objects
+ */
+fun NetworkAsteroidContainer.asDomainModel(): List<Asteroid> {
+    return asteroids.map {
+        Asteroid(
+            id = it.id,
+            codename = it.codename,
+            closeApproachDate = it.closeApproachDate,
+            absoluteMagnitude = it.absoluteMagnitude,
+            estimatedDiameter = it.estimatedDiameter,
+            relativeVelocity = it.relativeVelocity,
+            distanceFromEarth = it.distanceFromEarth,
+            isPotentiallyHazardous = it.isPotentiallyHazardous
+        )
+    }
+}
